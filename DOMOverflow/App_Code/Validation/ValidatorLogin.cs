3e80298186108bdafdec52a3098eab8ad9fc652d
Rob <hhy.yhh@live.nl>
2019-03-28 14:10:39 +0100

    Login, Logout & Register pages

---
+++ DOMOverflow/App_Code/Validation/ValidatorLogin.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Web;
+using System.Web.WebPages;
+
+namespace DOMOverflow {
+    public class ValidatorLogin : ValidatorDynamicErrorBase {
+        private string username, password;
+
+        public ValidatorLogin(string username, string password) : base("Error message not set.") {
+            this.username = username;
+            this.password = password;
+        }
+
+        protected override bool IsValid(HttpContextBase ctx, string value) {
+            string error;
+            bool success = DBManager.LoginUser(username, password, ctx.Session, out error);
+
+            UpdateErrorMessage(error);
+
+            return success;
+        }
+    }
+}
---
+++ DOMOverflow/App_Code/Validation/ValidatorRegister.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.WebPages;
+
+namespace DOMOverflow {
+    public class ValidatorRegister : ValidatorDynamicErrorBase {
+        private string username, password, email;
+
+        public ValidatorRegister(string username, string password, string email) : base("Error message not set.") {
+            this.username = username;
+            this.password = password;
+            this.email = email;
+        }
+
+        protected override bool IsValid(HttpContextBase ctx, string value) {
+            string error;
+            bool success = DBManager.RegisterUser(username, password, email, ctx.Session, out error);
+
+            UpdateErrorMessage(error);
+
+            return success;
+        }
+    }
+}
